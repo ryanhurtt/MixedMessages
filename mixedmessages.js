a814394235d8ce8957d73af1442f9ea835043f12
ryanhurtt <ryanhurtt@gmail.com>
2022-05-03 21:42:09 -0500

    Add functionality to create horoscope objects with Sign, Status, and Advice notes.

--- mixedmessages.js
+++ mixedmessages.js
@@ -1,6 +1,7 @@
-const returnSign = (birthDate) => {
+const returnSign = () => {
     const sign = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Saggitarius', 'Capricorn', 'Aquarius', 'Pisces'];
     
+    return sign[Math.floor(Math.random() * sign.length)];
 }
 
 const returnStatus = () => {
@@ -11,13 +12,27 @@ const returnStatus = () => {
     const statusAdj = statusAdjArr[Math.floor(Math.random() * statusAdjArr.length)]
     const statusNoun = statusNounArr[Math.floor(Math.random() * statusNounArr.length)]
     
-    return `You are experiencing ${statusAdj} ${statusNoun}.`;
+    return `${statusAdj} ${statusNoun}`;
 }
 
-const horoscopeFactory = (sign, arr2, arr3) => {
+const returnAdvice = () => {
+    const advice = ['trust no one', 'eliminate a bad habit', 'reach out to a loved one', 'make a healthy change', 'rob a bank', 'hit the gym', 'call your bookie']
 
+    return advice[Math.floor(Math.random() * advice.length)];
 }
+
+const horoscopeFactory = (sign, status, advice) => {
+    return {
+        sign: sign,
+        status: status,
+        advice: advice
+    };
+}
+
+let horoscopes = [];
 
 for (let i = 0; i < 4; i++) {
-    console.log(returnStatus());
+    horoscopes[i] = horoscopeFactory(returnSign(), returnStatus(), returnAdvice());
 }
+
+console.log(horoscopes);
